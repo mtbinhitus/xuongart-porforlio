14c59c3c839720ee347faf5217a852fb9e8ee742
Binh Mai <mtbinhitus@gmail.com>
2024-07-08 14:53:12 +0700

    feat(admin):sql seed

---
+++ seed.sql
@@ -0,0 +1,4 @@
+select * from behance.infor;
+select * from behance.image;
+insert into behance.infor (id, name, title, url_avatar, phone_title, phone_number, url_fb, address, project_views, appreciations, followers, follwings, url_banner, url_fanpage, cty_name, mst, location, url_message) values(1, "Nhan NT", "default", "default", "default", "default", "default", "default", 0, 0, 0, 0, "default", "default", "default", "default", "default", "default");
+select * from behance.tag;
